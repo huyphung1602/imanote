6c9a5ca591988f75f74dc83df76fd8e957c76664
huyphung1602 <huy.phung.sw@gmail.com>
2024-01-02 00:26:40 +0700

    Drag to draw the squares

--- components/NoteMain.vue
+++ components/NoteMain.vue
@@ -16,29 +16,50 @@ import Konva from 'konva';
 import { ref, onMounted } from 'vue';
 
 const log = console.log;
+const isNowDrawing = ref(false);
+
+let stage;
+let layer;
+let rect = null;
+
+const mousedownHandler = () => {
+  isNowDrawing.value = true;
+  rect = new Konva.Rect({
+    x: stage.getPointerPosition().x,
+    y: stage.getPointerPosition().y,
+    width: 0,
+    height: 0,
+    fill: 'lightblue',
+    stroke: 'blue',
+  })
+  layer.add(rect).batchDraw();
+};
+
+const mousemoveHandler = () => {
+  console.log(isNowDrawing.value);
+  if (!isNowDrawing.value) return false;
+  const newWidth = stage.getPointerPosition().x - rect.x();
+  const newHeight = stage.getPointerPosition().y - rect.y();
+  rect.width(newWidth).height(newHeight);
+  layer.batchDraw();
+};
+
+const mouseupHandler = () => {
+  isNowDrawing.value = false;
+};
 
 onMounted(() => {
   const diagram = document.getElementById('diagram');
-  const stage = new Konva.Stage({
+  stage = new Konva.Stage({
     height: diagram.clientHeight,
     width: diagram.clientWidth,
     container: diagram,
   });
-  const layer = new Konva.Layer();
-  const click = () => {
-    log(layer);
-    const rect = new Konva.Rect({
-      x: stage.getPointerPosition().x,
-      y: stage.getPointerPosition().y,
-      width: 20,
-      height: 20,
-      fill: 'lightblue',
-      stroke: 'blue',
-    })
-    layer.add(rect).batchDraw();
-  };
+  layer = new Konva.Layer();
 
   stage.add(layer);
-  stage.on('click', click);
+  stage.on('mousedown', mousedownHandler);
+  stage.on('mousemove', mousemoveHandler);
+  stage.on('mouseup', mouseupHandler);
 })
 </script>
